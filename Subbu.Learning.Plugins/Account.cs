724807e5fc31c2b430ef1389718df981b7256975
Subramanian <subramani.94@gmail.com>
2016-08-25 12:50:15 +0530

    Testing Pre Images / Post Images

---
+++ Subbu.Learning.Plugins/Account.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace Subbu.Learning.Plugins
+{
+    using System;
+    using Microsoft.Xrm.Sdk;
+
+    public class Account : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                if (serviceProvider == null)
+                {
+                    throw new ArgumentNullException("serviceProvider", "Service Provider is Null");
+                }
+
+                IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+                IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+                ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+                Entity target = (Entity)context.InputParameters["Target"];
+                Entity contact = new Entity("contact");
+                contact.Id = (target.GetAttributeValue<EntityReference>("primarycontactid")).Id;
+                contact["emailaddress1"] = target.GetAttributeValue<string>("name") + "@subbu.learnings.com";
+                service.Update(contact);
+            }
+            catch (Exception exception)
+            {
+                throw new InvalidPluginExecutionException(exception.Message);
+            }
+        }
+    }
+}
---
+++ Subbu.Learning.Plugins/Contact.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Subbu.Learning.Plugins
+{
+    using System;
+    using Microsoft.Xrm.Sdk;
+   
+    /// <summary>
+    /// Contact - Plugin
+    /// </summary>
+    public class Contact : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            try
+            {
+                if (serviceProvider == null)
+                {
+                    throw new ArgumentNullException("serviceProvider", "Service Provider is Null");
+                }
+
+                IOrganizationServiceFactory factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+                IOrganizationService service = factory.CreateOrganizationService(context.UserId);
+                ITracingService tracingService = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+                Entity target = (Entity)context.InputParameters["Target"];
+                if (target.Contains("emailaddress1"))
+                {
+                    Entity preImage = (Entity) context.PreEntityImages["PreImage"],
+                           postImage = (Entity) context.PostEntityImages["PostImage"];
+                    Entity updatingEntity = new Entity("contact");
+                    updatingEntity.Id = target.Id;
+                    updatingEntity["description"] = "Pre Image: Emailaddress1 - " + preImage.GetAttributeValue<string>("emailaddress1") + " Post Image: Emailaddress1 - " + postImage.GetAttributeValue<string>("emailaddress1");
+                    service.Update(updatingEntity);
+                    
+                }
+                if (target.Contains("mobilephone"))
+                {
+                    Entity account = new Entity("account");
+                    account["primarycontactid"] = target.ToEntityReference();
+                    account["name"] = target["mobilephone"];
+                    service.Create(account);
+                }
+                
+            }
+            catch (Exception exception)
+            {
+                throw new InvalidPluginExecutionException(exception.Message);
+            }
+        }
+    }
+}
